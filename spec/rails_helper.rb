8952ee13957992461a429b8969246ab7823ccbb7
izaxveta <izavetha@gmail.com>
2018-08-16 20:06:57 -0500

    Adds configuration settings for factory_bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,4 +76,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+  # config.generators do |g|
+  #   g.factory_bot dir: 'spec/factories'
+  # end
 end
