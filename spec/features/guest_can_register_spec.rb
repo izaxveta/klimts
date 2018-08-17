38464748bd76f57a4f4f5e7f7c85c9c727d03713
izaxveta <izavetha@gmail.com>
2018-08-16 22:50:06 -0500

    Adds spec for registering users

--- spec/features/guest_can_register_spec.rb
+++ spec/features/guest_can_register_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Guests' do
-  it 'can visit the landing page' do
-    visit '/'
+  before(:each) { visit '/' }
 
+  it 'can visit the landing page' do
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Klimt's")
     expect(page).to have_link("Register")
   end
 
   it 'can register for an account' do
-    visit '/'
     click_on "Register"
 
     expect(current_path).to eq(register_path)
@@ -26,5 +25,16 @@ RSpec.describe 'Guests' do
     expect(page).to have_content('success')
   end
 
-
+  it 'cannot register under an email that is already in use' do
+    create(:user, email: 'jessica.hamby@mail.com')
+    click_on "Register"
+    fill_in 'user_email', with: 'jessica.hamby@mail.com'
+    fill_in 'user_first_name', with: 'Jessica'
+    fill_in 'user_last_name', with: 'Hamby'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_on 'Register'
+    expect(current_path).to eq(register_path)
+    expect(page).to have_selector(:css, '.alert.alert-error')
+  end
 end
