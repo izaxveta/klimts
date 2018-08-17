5dc4d16e426cb881ec11a8f80b017cb00888696d
izaxveta <izavetha@gmail.com>
2018-08-16 19:55:58 -0500

    Adds actions for user signing in and creates current_user method to save user throughout session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if authenticated?(user, params)
+      set_session(user)
+      flash[:success] = 'Sign in successful!'
+      redirect_to dashboard_path
+    else
+      flash[:error] = 'Invalid credentials. Please try again.'
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    flash[:signout_success] = 'Sign out successful.'
+    redirect_to root_path
+  end
+
+  private
+    def authenticated?(user, params)
+      user && user.authenticate(params[:session][:password])
+    end
+
+    def set_session(user)
+      session[:user] = user.id
+    end
+end
