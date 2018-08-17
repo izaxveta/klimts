c5767f1148e88e302c4d083dcf7dd3ab54a83c60
izaxveta <izavetha@gmail.com>
2018-08-16 22:26:34 -0500

    Adds spec for guest registration and user sign out

---
+++ spec/features/authenticated_user_can_sign_out_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Authenticated users' do
+  let(:user) { create(:user) }
+  it 'can sign out and return to the landing page' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit dashboard_path
+    click_on 'Sign Out'
+    expect(current_path).to eq(root_path)
+    expect(page).to have_selector(:css, '.alert.alert-signout_success')
+  end
+end
