cd8c02a06ed4a0310e61e2e1e0e33b9f3cfd88fd
izaxveta <izavetha@gmail.com>
2018-08-23 17:47:09 -0500

    Reorganizes and categorizes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
+  # ADMIN
   get '/admin/dashboard', to:'admin/dashboard#index', as: 'admin_dashboard'
 
+  # DEFAULT
+  get '/dashboard', to: 'dashboard#index', as: 'dashboard'
+
+  # GLOBAL
   resources :users, only: [:index, :create]
   get  '/register', to: 'users#new',    as: 'register'
   get  '/signin',   to: 'sessions#new', as: 'signin'
   post '/signin',   to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy', as: 'signout'
-
-  get '/dashboard', to: 'dashboard#index', as: 'dashboard'
 end
