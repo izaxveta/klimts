3147923f9c114972d2995d99436eb7cd8997f014
izaxveta <izavetha@gmail.com>
2018-08-28 18:23:06 -0500

    Updates enum values for product status

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
-  enum status: [:'Pre Order', :'In Stock', :'Out of Stock', :'Retired']
+
+  enum status: [:pre_order, :in_stock, :out_of_stock, :retired]
+
+  def self.total_cash_on_hand
+    in_stock.reduce(0) { |total, product| total += (product.quantity * product.price) }
+  end
+
+  def self.products_grouped_by_statuses(statuses)
+    products = Hash.new
+    statuses.each{ |status| products[status] = Product.where(status: status) }
+    return products
+  end
+
+  def self.display_status(status)
+    status.gsub('_', ' ').titleize
+  end
 end
