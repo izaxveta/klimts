b263d325d293ff7ba8439a5bf35a0c93b6b2b2c2
izaxveta <izavetha@gmail.com>
2018-08-28 18:25:59 -0500

    Adds breadcrumb_url to navigate to any previous page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :breadcrumb_url
 
   def current_user
     @current_user ||= User.find(session[:user]) if session[:user]
+  end
+
+  def breadcrumb_url
+    request.env["HTTP_REFERER"]
   end
 end
